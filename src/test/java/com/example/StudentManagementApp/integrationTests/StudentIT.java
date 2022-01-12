d4555c29ebcb8047fe5f9dd7da0f04c49c641ff9
emmanuel_okonkwo <chukwujekwuekene2017@gmail.com>
2022-01-12 05:53:52 -0800

    Implemented integration testing to test the overall functionality of the application when adding a new student to the application.

---
+++ src/test/java/com/example/StudentManagementApp/integrationTests/StudentIT.java
@@ -0,0 +1,58 @@
+package com.example.StudentManagementApp.integrationTests;
+
+import com.example.StudentManagementApp.student.Gender;
+import com.example.StudentManagementApp.student.Student;
+import com.example.StudentManagementApp.student.StudentRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application-it.properties"
+)
+@AutoConfigureMockMvc
+public class StudentIT {
+    // Note that the class should be prefixed with IT because the maven plugin would only look run integration tests on classes that end with IT
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private StudentRepo studentRepo;
+
+    @Test
+    void canAddNewStudent() throws Exception {
+        // Given
+        Student student = new Student(
+                "Mathews",
+                "abscd@gmail.com",
+                Gender.FEMALE
+        );
+        // When
+        // Note: The objectMapper.writeValueAsString() function converts student to a JSON payload content
+        ResultActions resultActions = mockMvc
+                .perform(post("/api/v1/students")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(student)));
+        // Then
+        resultActions.andExpect(status().isOk());
+        List<Student> students = studentRepo.findAll();
+        assertThat(students).usingElementComparatorIgnoringFields("id").contains(student);
+    }
+}
--- src/test/java/com/example/StudentManagementApp/student/StudentRepoTest.java
+++ src/test/java/com/example/StudentManagementApp/student/StudentRepoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 class StudentRepoTest {
 
+    @Autowired
     private StudentRepo insideTest;
 
     // The two methods below represent testing for the Query that was created in the database
